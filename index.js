a5f4d5463559faf435519a5e5ee76fe1be3a6154
sumaluma <aaronsouthard07@gmail.com>
2025-02-02 22:45:14 -0500

    commit

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-const express = require('express');
-             const server = express();
-             const port = 3000;
+import { routeHello, routeAPINames } from "./routes.js";
+import express from "express";
+
+const server = express();
+const port = 3000;
     
- server.get('/hello', function (req, res) {
-     res.send('Hello World! My name is Aaron Southard')
+server.get('/hello', function (req, res) {
+    const response = routeHello(req, res);
+    res.send(response);
 });
-             server.listen(port, function () {
-                 console.log('Listening on ' + port);
+
+server.get("/api/names", async function (req, res) { 
+    let response;
+    try {
+        response = await routeAPINames(req, res);
+    } catch (err){
+        console.log(err);
+    }
+    res.send(response);
+});
+
+server.listen(port, function () {
+        console.log("Listening on " + port);
 });
